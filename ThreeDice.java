8b60d1f8be645d32ef306d8a4a8d25ab693fc1df
Steven Walley <swalley2626@yahoo.com>
2016-05-04 20:00:08 -0700

    Added files via upload

--- ThreeDice.java
+++ ThreeDice.java
@@ -1,13 +1,11 @@
 
 public class ThreeDice {
 	
-	private int D1 = 1 + (int)(Math.random() *6);
-	private int D2 = 1 + (int)(Math.random() *6);
-	private int D3 = 1 + (int)(Math.random() *6);
+	private Dice2 D1, D2, D3;
 	
 	public int Throw()
 	{
-		int average = (D1 + D2 + D3)/3;
+		int average = (D1.Throw(1) + D2.Throw(1) + D3.Throw(1))/3;
 		return average;
 	}
 
